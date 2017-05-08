ac49f50d52c51a97498eb0aff397a6e5145e3b2b
sergey <sergey@192.168.0.13>
2017-05-07 18:52:14 -0700

    get day of week by the date

---
+++ getDayofWeek/src/main/java/getDayofWeek/App.java
@@ -0,0 +1,14 @@
+package getDayofWeek;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class App {
+
+	public static void main(String[] args) {
+		Calendar calendar = Calendar.getInstance();
+        calendar.set(2015, 8, 05);
+        System.out.println("" + calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US));
+	}
+
+}
